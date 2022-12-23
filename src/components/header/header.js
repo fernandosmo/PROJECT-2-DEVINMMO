753beb3862e1fe8f2b5cd6e79002f87f6c364e3b
Fernando Silva <fernandosmo@icloud.com>
2022-12-23 18:51:35 -0300

    update routes on header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { Container } from './styles';
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    
     <Container>
       <div className="header">
         <div className="logo">
-         <h2 onClick={() => navigate(`/`)}>DEVinMMO</h2>
+          <h2 onClick={() => navigate(`PROJECT-2-DEVINMMO/`)}>DEVinMMO</h2>
         </div>
         <div className="homeBtn">
-          <button className="button" onClick={() => navigate("/")}>Home</button>
-          <button className="button" onClick={() => navigate("/news")}>News</button>
-          </div>
-        </div>        
+          <button
+            className="button"
+            onClick={() => navigate('PROJECT-2-DEVINMMO/')}>
+            Home
+          </button>
+          <button
+            className="button"
+            onClick={() => navigate('PROJECT-2-DEVINMMO/news')}>
+            News
+          </button>
+        </div>
+      </div>
     </Container>
   );
-}
+};
 
 export default Header;
